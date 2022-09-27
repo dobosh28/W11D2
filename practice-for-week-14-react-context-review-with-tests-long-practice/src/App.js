078067972302577715715e74ca4ce814f295d11e
Yosyp Dobosh <joseph.dobosh@gmail.com>
2022-09-27 21:03:59 +0200

    test 2

--- practice-for-week-14-react-context-review-with-tests-long-practice/src/App.js
+++ practice-for-week-14-react-context-review-with-tests-long-practice/src/App.js
@@ -1,9 +1,13 @@
 import coffeeBeans from "./mockData/coffeeBeans.json";
+import SetCoffeeBean from "./components/SetCoffeeBean";
+import SelectedCoffeeBean from "./components/SelectedCoffeeBean";
 
 function App() {
   return (
     <>
       <h1>Welcome to Coffee App</h1>
+      <SelectedCoffeeBean/>
+      <SetCoffeeBean coffeeBeans={coffeeBeans}/>
     </>
   );
 }
--- practice-for-week-14-react-context-review-with-tests-long-practice/src/components/SetCoffeeBean.js
+++ practice-for-week-14-react-context-review-with-tests-long-practice/src/components/SetCoffeeBean.js
@@ -1,9 +1,14 @@
+import { useCoffeeContext } from "../context/CoffeeContext"; 
+
 const SetCoffeeBean = ({ coffeeBeans }) => {
+  const { setCoffeeBean } = useCoffeeContext();
+  //debugger
   return (
     <div className="set-coffee-bean">
       <h2>Select a Coffee Bean</h2>
       <select
         name="coffee-bean"
+        onChange={ (e) => setCoffeeBean(e.target.key) }
       >
         {coffeeBeans.map(bean => (
           <option
